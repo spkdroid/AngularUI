75923263f2f523b309b5eee6ac2ad110719c2a20
Ramkumar Velmurugan <vrk1990@outlook.com>
2017-06-11 17:23:27 -0300

    -Compose module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-
 import {routes} from './app.router'
-
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ServiceComponent } from './service/service.component';
 import { LogoutComponent } from './logout/logout.component';
 import { InboxComponent } from './inbox/inbox.component';
+import { ComposeComponent } from './compose/compose.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +16,8 @@ import { InboxComponent } from './inbox/inbox.component';
     AboutComponent,
     ServiceComponent,
     LogoutComponent,
-    InboxComponent
+    InboxComponent,
+    ComposeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -5,12 +5,15 @@ import { AppComponent} from './app.component';
 import { AboutComponent} from './about/about.component';
 import { ServiceComponent} from './service/service.component';
 import { InboxComponent } from './inbox/inbox.component';
+import { ComposeComponent } from './compose/compose.component';
+
 
 export const router: Routes = [
 { path: '',redirectTo:"inbox",pathMatch:'full'},
 { path: 'about',component: AboutComponent},
 { path: 'service',component: ServiceComponent},
-{ path: 'inbox',component: InboxComponent}
+{ path: 'inbox',component: InboxComponent},
+{ path: 'compose',component: ComposeComponent}
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
